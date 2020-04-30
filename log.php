b849b1456c5a059d0fdeb2bf4cde7bdea7852331
Brianna Price <briannaprice@Briannas-MacBook-Air.local>
2020-04-30 13:06:49 -0500

    deleted unneeded html

--- log.php
+++ log.php
@@ -22,4 +22,15 @@ if(isset($_POST['enter'])){
         echo '<span class="error">Please type in a name</span>';
     }
 }
+
+if(isset($_GET['logout'])){ 
+     
+    //Simple exit message
+    $fp = fopen("log.html", 'a');
+    fwrite($fp, "<div class='msgln'><i>User ". $_SESSION['name'] ." has left the chat session.</i><br></div>");
+    fclose($fp);
+     
+    session_destroy();
+    header("Location: index.php"); //Redirect the user
+}
 ?>
